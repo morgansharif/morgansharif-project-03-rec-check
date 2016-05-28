370dab9aae2387fc03dc272fffb43a0156f05289
morgansharif <morgan.sharif@gmail.com>
2016-05-28 16:33:44 -0700

    added default map pos. in games index

--- public/scripts/controllers/games-index.controller.js
+++ public/scripts/controllers/games-index.controller.js
@@ -3,6 +3,9 @@ function GamesIndexController ($http) {
   var vm = this;
   vm.games = [];
 
+  // set default map position over bay area
+  vm.map = { center: { latitude: 37.78, longitude: -122.44 }, zoom: 8 };
+
   query(); // fetch all the games (index)
 
   ////
