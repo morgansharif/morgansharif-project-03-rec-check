6e190a8c515ad5c233a592f63008009c1452acb5
morgansharif <morgan.sharif@gmail.com>
2016-06-01 14:35:56 -0700

    filter vm.games by date

--- public/scripts/controllers/games-index.controller.js
+++ public/scripts/controllers/games-index.controller.js
@@ -2,13 +2,34 @@ GamesIndexController.$inject = ["$http"]; // minification protection
 function GamesIndexController ($http) {
   var vm = this;
   vm.games = [];
-  vm.filterDate = Date.now();
+  vm.filteredGames = [];
+  vm.filterDate = [new Date(), new Date()];
   vm.dateList = [];
   // set default map position over bay area
   vm.map = { center: { latitude: 37.78, longitude: -122.44 }, zoom: 8 };
   query(); // fetch all the games (index)
 
   next_30_days();
+
+  // filter function
+  vm.dateFilter = dateFilter;
+
+  ////
+
+  function dateFilter(games, dateRange) {
+      var startDate   = new Date(dateRange[0]),
+          endDate     = new Date(dateRange[1]),
+          result      = [];
+      for (var i=0, len = games.length; i < len; i++) {
+        gameDate = new Date(games[i].start);
+        if (startDate < gameDate && gameDate < endDate) {
+           result.push(games[i]);
+        }
+      }
+      console.log(result);
+      vm.filteredGames = result;
+  }
+
 
   // builds array of start/end times for the next 30 days to be used for filter
   function next_30_days(){
@@ -19,24 +40,23 @@ function GamesIndexController ($http) {
     var month = today.getMonth();
     var date = today.getDate();
     for(var i=0; i<30; i++){
-          var this_day= new Date(year, month, date + i);
-          var day_end = new Date(year, month, date + i, 23, 59, 59, 999);
-          tempArr.push([this_day, day_end]);
-          // console.log("___day "+ (i + 1) +"___");
-          // console.log("<0> " + this_day);
-          // console.log("<1> " + day_end);
+      var this_day= new Date(year, month, date + i);
+      var day_end = new Date(year, month, date + i, 23, 59, 59, 999);
+      tempArr.push([this_day, day_end]);
+      // console.log("___day "+ (i + 1) +"___");
+      // console.log("<0> " + this_day);
+      // console.log("<1> " + day_end);
     }
     vm.dateList = tempArr;
     vm.filterDate= tempArr[0];
   }
-
-  ////
 
   function query() {
     $http
       .get('/api/games')
       .then(function onSuccess(response) {
         vm.games = response.data;
+        dateFilter(vm.games, vm.filterDate);
       });
   }
 }
