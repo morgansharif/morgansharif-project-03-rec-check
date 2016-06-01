b12b240879437a87ec03833be92370df630c96e8
morgansharif <morgan.sharif@gmail.com>
2016-05-31 21:56:00 -0700

    set min date to current time

--- public/scripts/controllers/games-new.controller.js
+++ public/scripts/controllers/games-new.controller.js
@@ -5,13 +5,10 @@ function GamesNewController ($location, $http) {
   vm.map = { center: {latitude: 37.78, longitude: -122.44 }, zoom: 12 };
   vm.autoAddress = {};
   vm.game = {}; // form data
-  //
-  var now = new Date();
-  vm.dateNow = now.toISOString();
-  vm.cansubmit = cansubmit;
-  function cansubmit(){
-  return vm.autoAddress.hasOwnProperty("geometry");
-}
+  // set min date to current time
+  var currTime = new Date();
+  vm.minDate = currTime.toISOString();
+
 
   function create() {
     // prevent creating a game without autocomplted coordinates and name values
